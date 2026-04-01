763f644e5ca6594ed8c690a90f6cdd90cba30d08
Bappy Ahmmed <itznobita958@gmail.com>
2026-04-01 18:36:28 +0600

    minor bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
